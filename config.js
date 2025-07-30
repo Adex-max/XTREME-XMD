43755ac988821cfa90388abbb31249a172aba44e
Adex-max <adexffx@gmail.com>
2025-07-30 08:07:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~rg5mCYhL#0CG6rUL93hCFypkaEKHqUbWyXHoWPWENUd7Ucb06Wfs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
